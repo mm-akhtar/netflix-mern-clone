c1b375cd0a807f251378d6fe7fff4fb1d0ecc3f9
Md Masrur Akhtar <kkakhtar143@gmail.com>
2021-05-25 22:44:35 +0530

    solved security

--- src/Connection/nexios.js
+++ src/Connection/nexios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://akhtar-netflix-api.herokuapp.com",
+  baseURL: process.env.Base_URL,
 });
 
 export default instance;
--- src/Connection/requests.js
+++ src/Connection/requests.js
@@ -1,4 +1,4 @@
-const API_KEY = "9b1989f0c92bb9204b90b1a263232d21";
+const API_KEY = process.env.API_KEY;
 
 const requests = {
   fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
